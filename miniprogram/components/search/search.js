716c2c302dcddd4ffff490c4867e0af517a79e8b
WongTsing <892789755@qq.com>
2020-01-08 19:09:03 +0800

    修改样式，外部传入到组件内部的样式些=写法

--- miniprogram/components/search/search.js
+++ miniprogram/components/search/search.js
@@ -4,8 +4,16 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    placeholder:{
+      type:String,
+      value:"请输入关键字"
+    }
   },
+  //接收外部传入外部样式
+  externalClasses:[
+    'iconfont',
+    'icon-search'
+  ],
 
   /**
    * 组件的初始数据
